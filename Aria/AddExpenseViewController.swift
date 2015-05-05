fe5f984188659b7be5f359453ac51aec3eea959d
Guy <harpakguy@gmail.com>
2015-05-05 23:22:45 +0300

    Added persistent DB support using SQLite

--- Aria/AddExpenseViewController.swift
+++ Aria/AddExpenseViewController.swift
@@ -14,7 +14,6 @@ class AddExpenseViewController : UIViewController {
     @IBOutlet weak var amount_txt: UITextField!
     @IBOutlet weak var enter_btn: UIButton!
     
-    
     @IBAction func enterPressed(sender: AnyObject) {
         sharedDBManager.addExpense( (amount_txt.text as NSString).doubleValue, InCategory: Category.Supermarket)
     }
--- Aria/ExpenseDB.swift
+++ Aria/ExpenseDB.swift
@@ -23,11 +23,16 @@ class Expense :NSObject
         self.timestamp = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: .MediumStyle, timeStyle: .ShortStyle)
         self.category = category
     }
+    
+    func getTimestamp() -> NSString {
+        return timestamp
+    }
 }
 
 
 class ExpenseDBManager
 {
+    var database_path: String;
     var expenseArray: [Expense] = [];
 /*    var database: FMDatabase? = nil
 
@@ -40,6 +45,32 @@ class ExpenseDBManager
     
     
     init () {
+        
+        let filemgr = NSFileManager.defaultManager()
+        let dirPaths =
+        NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
+            .UserDomainMask, true)
+        
+        let docsDir = dirPaths[0] as! String
+        
+        database_path = docsDir.stringByAppendingPathComponent("expenseDB.db")
+        
+        if !filemgr.fileExistsAtPath(database_path as String) {
+            
+            let database_ref = FMDatabase(path: database_path as String)
+            if database_ref == nil {
+                println("Error: \(database_ref.lastErrorMessage())")
+            }
+            if database_ref.open() {
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS EXPENSES (ID INTEGER PRIMARY KEY AUTOINCREMENT, AMOUNT REAL, CATEGORY TEXT, DATE TEXT)"
+                if !database_ref.executeStatements(sql_stmt) {
+                    println("Error: \(database_ref.lastErrorMessage())")
+                }
+                database_ref.close()
+            } else {
+                println("Error: \(database_ref.lastErrorMessage())")
+            }
+        }
     }
     
     func getExpenseArray() -> [Expense] {
@@ -48,15 +79,48 @@ class ExpenseDBManager
     
     func getDoubleExpenseArray()-> [Double] {
         var expense_double_array : [Double] = []
-        for expense in self.expenseArray {
-            expense_double_array.append(expense.amount)
+        let database_ref = FMDatabase(path: database_path as String)
+        
+        if database_ref.open() {
+            let querySQL = "SELECT amount FROM EXPENSES"
+            
+            let results:FMResultSet? = database_ref.executeQuery(querySQL,
+                withArgumentsInArray: nil)
+            
+            while results?.next() == true {
+                expense_double_array.append(results!.doubleForColumn("amount"))
+            }
+            database_ref.close()
+        } else {
+            println("Error: \(database_ref.lastErrorMessage())")
         }
         return expense_double_array
     }
     
     func addExpense (amount:Double, InCategory category:Category) {
-        self.expenseArray.append(Expense(id: self.expenseArray.count, amount:amount, InCategory: category))
+        let added_expense = Expense(id: self.expenseArray.count, amount:amount, InCategory: category)
+        self.expenseArray.append(added_expense)
+        let database_ref = FMDatabase(path: database_path as String)
+        
+        if database_ref.open() {
+            // TODO: get category from parameter & get date
+            let insertSQL = "INSERT INTO EXPENSES (amount, category, date) VALUES ('\(amount)', 'Fuel', '\(added_expense.getTimestamp())')"
+            
+            let result = database_ref.executeUpdate(insertSQL,
+                withArgumentsInArray: nil)
+            
+            if !result {
+                println ( "Failed to add expense to DB")
+                println("Error: \(database_ref.lastErrorMessage())")
+            } else {
+                println ("Expense Added")
+            }
+        } else {
+            println("Error: \(database_ref.lastErrorMessage())")
+        }
     }
+    
+    
 }
 
 let sharedDBManager = ExpenseDBManager()
